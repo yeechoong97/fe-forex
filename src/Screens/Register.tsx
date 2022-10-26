051971b0ac5ef68e6001e7fe789f140bd01c9178
Yee <yeechoong97@gmail.com>
2022-10-26 21:17:28 +0800

    Completed Register Page

--- src/Screens/Register.tsx
+++ src/Screens/Register.tsx
@@ -1,9 +1,10 @@
-import { Step, StepLabel, Stepper } from '@mui/material'
+import { CircularProgress, Step, StepConnector, stepConnectorClasses, StepIconProps, StepLabel, Stepper, styled } from '@mui/material'
 import React, { ChangeEvent, ChangeEventHandler, FunctionComponent, useState } from 'react'
 import { Link } from 'react-router-dom'
 import ErrorMessage from '../Components/ErrorMessage'
 import TextBox from '../Components/TextBox'
 import { OnChangeHandler, UserDetails } from '../interfaces'
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 const InitialAccount: UserDetails = {
     username: "",
@@ -12,6 +13,10 @@ const InitialAccount: UserDetails = {
     email: "",
     password: "",
     confirm_password: "",
+}
+
+interface LoadingType {
+    load: boolean
 }
 
 const PersonalSection: FunctionComponent<OnChangeHandler> = ({ setAction }) => (
@@ -30,11 +35,83 @@ const CredentialsSection: FunctionComponent<OnChangeHandler> = ({ setAction }) =
     </>
 )
 
-const ProcessSection = () => (
-    <>
-        <p>Last PAge</p>
-    </>
-)
+const ProcessSection: FunctionComponent<LoadingType> = ({ load }) => {
+
+    return (
+        <div className='flex justify-center flex-col items-center py-3'>
+            {
+                load ? (<>
+                    <CircularProgress />
+                    <span className='my-3 text-slate-500 text-sm'>Please wait</span>
+                </>) :
+                    (<>
+                        <CheckCircleIcon className="text-green-500 text-2xl" fontSize='large' />
+                        <span className='my-3 text-slate-500 text-sm'>Registration Success</span>
+                    </>)
+            }
+        </div >)
+}
+
+const QontoConnector = styled(StepConnector)(({ theme }) => ({
+    [`&.${stepConnectorClasses.alternativeLabel}`]: {
+        top: 10,
+        left: 'calc(-50% + 16px)',
+        right: 'calc(50% + 16px)',
+    },
+    [`&.${stepConnectorClasses.active}`]: {
+        [`& .${stepConnectorClasses.line}`]: {
+            borderColor: '#90caf9',
+        },
+    },
+    [`&.${stepConnectorClasses.completed}`]: {
+        [`& .${stepConnectorClasses.line}`]: {
+            borderColor: '#90caf9',
+        },
+    },
+    [`& .${stepConnectorClasses.line}`]: {
+        borderColor: theme.palette.mode === 'dark' ? theme.palette.grey[800] : '#eaeaf0',
+        borderTopWidth: 3,
+        borderRadius: 1,
+    },
+}));
+
+const QontoStepIconRoot = styled('div')<{ ownerState: { active?: boolean } }>(
+    ({ theme, ownerState }) => ({
+        color: theme.palette.mode === 'dark' ? theme.palette.grey[700] : '#eaeaf0',
+        display: 'flex',
+        height: 22,
+        alignItems: 'center',
+        ...(ownerState.active && {
+            color: '#0ea5e9',
+        }),
+        '& .QontoStepIcon-completedIcon': {
+            color: '#4ade80',
+            zIndex: 1,
+            fontSize: 18,
+        },
+        '& .QontoStepIcon-circle': {
+            width: 8,
+            height: 8,
+            borderRadius: '50%',
+            backgroundColor: 'currentColor',
+        },
+    }),
+);
+
+const QontoStepIcon = (props: StepIconProps) => {
+    const { active, completed, className } = props;
+
+    return (
+        <QontoStepIconRoot ownerState={{ active }} className={className}>
+            {completed ? (
+                <CheckCircleIcon className="QontoStepIcon-completedIcon" />
+            ) : (
+                <div className="QontoStepIcon-circle" />
+            )}
+        </QontoStepIconRoot>
+    );
+}
+
 
 const RegistrationSteps: string[] = ["Personal", "Credentials", "Success"];
 
@@ -43,15 +120,23 @@ const Register = () => {
     const [accountDetails, setAccountDetails] = useState<UserDetails>(InitialAccount);
     const [error, setError] = useState(false);
     const [currentStep, setCurrentStep] = useState(0);
+    const [loading, setLoading] = useState(true);
 
     const setAction = (event: ChangeEvent<HTMLInputElement>) => {
         const { value, name } = event.target;
         const existingAccount = accountDetails;
         accountDetails[name] = value;
         setAccountDetails(existingAccount);
-        console.log(currentStep);
     }
 
+    const submitForm = () => {
+        setCurrentStep(currentStep + 1);
+        setTimeout(() => {
+            setLoading(!loading);
+            setCurrentStep(currentStep + 2);
+        }, 2000);
+        //Todo Redirect back to Login after done register ( in few seconds);
+    }
 
     return (
         <div className='w-screen h-screen bg-slate-200 items-center justify-center flex'>
@@ -62,10 +147,10 @@ const Register = () => {
                     </div>
                 </div>
                 <hr />
-                <Stepper activeStep={currentStep} alternativeLabel className='py-3' >
+                <Stepper activeStep={currentStep} alternativeLabel className='py-3' connector={<QontoConnector />} >
                     {RegistrationSteps.map((label: string) => (
                         <Step key={label}>
-                            <StepLabel>{label}</StepLabel>
+                            <StepLabel StepIconComponent={QontoStepIcon}>{label}</StepLabel>
                         </Step>
                     ))}
                 </Stepper>
@@ -77,7 +162,7 @@ const Register = () => {
                 {
                     currentStep === 0 ? (<PersonalSection setAction={setAction} />) :
                         currentStep === 1 ? (<CredentialsSection setAction={setAction} />) :
-                            (<ProcessSection />)
+                            (<ProcessSection load={loading} />)
                 }
 
                 {/* Submit Action - Progress Loading Animation -   */}
@@ -86,10 +171,14 @@ const Register = () => {
                 <div className="flex flex-col justify-center items-center pb-10">
                     {
                         currentStep < 1 ? (<button className="bg-sky-500 hover:bg-sky-700 px-5 py-2.5 text-sm leading-5 rounded-md font-semibold text-white w-1/2 mt-6" onClick={() => setCurrentStep(currentStep + 1)}>Next</button>) :
-                            (<button className="bg-sky-500 hover:bg-sky-700 px-5 py-2.5 text-sm leading-5 rounded-md font-semibold text-white w-1/2 mt-6" onClick={() => { }}>Submit</button>)
+                            currentStep < 2 ? (<button className="bg-sky-500 hover:bg-sky-700 px-5 py-2.5 text-sm leading-5 rounded-md font-semibold text-white w-1/2 mt-6" onClick={submitForm}>Submit</button>) : <></>
                     }
-                    {currentStep === 1 && (<button className="bg-slate-100 hover:bg-slate-200 px-5 py-2.5 text-sm leading-5 rounded-md font-semibold text-slate-500 w-1/2 mt-3" onClick={() => setCurrentStep(currentStep - 1)}>Back</button>)}
-                    <span className='text-slate-400 text-sm my-3 sm:text-xs'>Have account? <Link to={"/login"} className='text-blue-500'>Login Here</Link></span>
+                    {currentStep === 1 && (<button className="bg-slate-100 hover:bg-slate-200 px-5 py-2.5 text-sm leading-5 rounded-md font-semibold text-slate-500 w-1/2 mt-3" onClick={() => setCurrentStep(currentStep - 1)}>Back</button>
+                    )}
+                    {
+                        currentStep < 2 && (<span className='text-slate-400 text-sm my-3 sm:text-xs'>Have account? <Link to={"/login"} className='text-blue-500'>Login Here</Link></span>
+                        )
+                    }
                 </div>
             </div>
         </div>
