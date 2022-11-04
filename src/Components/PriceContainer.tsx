fa26f46f392491e080a2e27244acd30e0d7b5846
Yee <yeechoong97@gmail.com>
2022-11-04 21:08:37 +0800

    Completed Table Design

--- src/Components/PriceContainer.tsx
+++ src/Components/PriceContainer.tsx
@@ -12,8 +12,8 @@ const PriceContainer: FunctionComponent<PriceProps> = (currency, { currentInstru
             </div>
             <hr className=' border-stone-300' />
             <div className='flex justify-center w-full h-2/3 relative'>
-                <div className='w-1/2 text-center  md:text-xl text-slate-600'>{currency.buy}</div>
-                <div className='w-1/2 text-center md:text-xl text-slate-600 border-l border-stone-300'>{currency.sell}</div>
+                <div className='w-1/2 text-center py-1 md:text-base text-slate-700'>{currency.buy}</div>
+                <div className='w-1/2 text-center py-1 md:text-base text-slate-700 border-l border-stone-300'>{currency.sell}</div>
                 <div className='h-5 w-20 bg-neutral-50 absolute bottom-0 rounded-sm text-sm text-slate-600 text-center border-x border-t border-stone-300'>
                     {currency.pips}
                 </div>
--- src/Components/TradeAction.tsx
+++ src/Components/TradeAction.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const TradeAction = () => {
     return (
-        <div className='w-1/6 bg-slate-50 flex flex-col justify-center px-4 py-4 space-y-3 border-r'>
+        <div className='w-1/6 bg-slate-50 flex flex-col justify-center px-4 py-4 space-y-3 border-r rounded-l-lg'>
             <div className='flex-col flex'>
                 <span className='text-blue-400 text-sm text-center'>Buy with </span>
                 <input type="button" className='bg-blue-400 text-white text-xl py-2 rounded-full mt-1' value="1711.11" />
