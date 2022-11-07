ff5cca2d0296802e5008d8985e5f5c4eb87b7c01
Yee <yeechoong97@gmail.com>
2022-11-07 20:27:33 +0800

    Updated the Table Components

--- src/Components/TradeAction.tsx
+++ src/Components/TradeAction.tsx
@@ -6,12 +6,12 @@ const TradeAction = () => {
         <div className='w-1/6 bg-slate-50 flex flex-col justify-center px-4 py-4 space-y-3 border-r rounded-l-lg'>
             <div className='flex-col flex'>
                 <span className='text-blue-400 text-sm text-center'>Buy with </span>
-                <input type="button" className='bg-blue-400 text-white text-xl py-2 rounded-full mt-1' value="1711.11" />
+                <input type="button" className='bg-blue-400 text-white text-xl py-2 rounded-full mt-1 cursor-pointer' value="1711.11" />
             </div>
             <Divider />
             <div className='flex-col flex'>
                 <span className='text-red-400 text-sm text-center'>Sell with </span>
-                <input type="button" className='bg-red-400 text-white text-xl py-2 rounded-full mt-1' value="1711.11" />
+                <input type="button" className='bg-red-400 text-white text-xl py-2 rounded-full mt-1 cursor-pointer' value="1711.11" />
             </div>
             <Divider />
             <div className='flex-col flex'>
---
+++ src/Components/TradeRecords.tsx
@@ -0,0 +1,102 @@
+import React from 'react'
+
+const TradeRecords = () => {
+    return (
+        <div className='w-5/6 overflow-x-auto rounded-tr-lg'>
+            <table className="w-full text-xs text-left text-slate-600 table-auto">
+                <thead className=" text-slate-700 uppercase bg-slate-100 border-b sticky top-0">
+                    <tr className='divide-x'>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <th scope="col" className="pt-3 text-center" colSpan={3}>
+                            Profit In
+                            <hr />
+                        </th>
+                        <td></td>
+                    </tr>
+                    <tr className='divide-x'>
+                        <th scope="col" className="py-2 px-3">
+                            Date
+                        </th>
+                        <th scope="col" className="py-2 px-3">
+                            Pair
+                        </th>
+                        <th scope="col" className="py-2 px-3">
+                            Type
+                        </th>
+                        <th scope="col" className="py-2 px-3">
+                            Margin
+                        </th>
+                        <th scope="col" className="py-2 px-3">
+                            Units
+                        </th>
+                        <th scope="col" className="py-2 px-3">
+                            Enter
+                        </th>
+                        <th scope="col" className="py-2 px-3">
+                            Current
+                        </th>
+                        <th scope="col" className="py-2 px-3">
+                            USD
+                        </th>
+                        <th scope="col" className="py-2 px-3">
+                            Spread
+                        </th>
+                        <th scope="col" className="py-2 px-3">
+                            %
+                        </th>
+                        <th scope="col" className="py-2 px-3">
+                            Action
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="bg-white border-b hover:bg-slate-200 hover:cursor-pointer divide-x hover:divide-slate-300 hover:border-slate-300 hover:border-t hover:border-r">
+                        <td className="py-3 px-3">
+                            11/11/2022
+                            <br />
+                            11:23 PM
+                        </td>
+                        <td className="py-3 px-3">
+                            EUR/USD
+                        </td>
+                        <td className="py-3 px-3">
+                            Short
+                        </td>
+                        <td className="py-3 px-3">
+                            1000
+                        </td>
+                        <td className="py-3 px-3">
+                            30,000
+                        </td>
+                        <td className="py-3 px-3">
+                            121.223
+                        </td>
+                        <td className="py-3 px-3">
+                            120.12
+                        </td>
+                        <td className="py-3 px-3">
+                            30.2
+                        </td>
+                        <td className="py-3 px-3">
+                            1.2
+                        </td>
+                        <td className="py-3 px-3">
+                            10.11
+                        </td>
+                        <td className="py-3 px-3 text-center">
+                            <i className="fa-solid fa-chevron-right"></i>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default TradeRecords
