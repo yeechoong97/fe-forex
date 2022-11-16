4edaeebc3464c8961eeb2ce13f7b5c88f5e56a33
Yee <yeechoong97@gmail.com>
2022-11-16 22:19:30 +0800

    Completed Modal Design

--- src/Components/TextBox.tsx
+++ src/Components/TextBox.tsx
@@ -3,7 +3,7 @@ import { TextBoxType } from '../interfaces';
 
 const TextBox: FunctionComponent<TextBoxType> = ({ title, placeholder, type, setAction, value, autoFocus = false }) => {
     return (
-        <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col justify-center items-center w-full">
             <span className='text-slate-500 text-sm w-1/2'>{placeholder}</span>
             <input className="w-1/2 mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block rounded-md sm:text-sm focus:ring-1" type={type} name={title} placeholder={placeholder} required autoFocus={autoFocus} onChange={setAction} value={value} />
         </div>
